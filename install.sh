b5d52df8536510677600235454b1edc93e290403
kbehafarin@gmail.com <kbehafarin@gmail.com>
2013-10-22 22:02:29 -0700

    install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ use warnings;
 
 my $basedir = '/salzh';
 my $giturl  = '';
-if ( -d "$basedir/elastix") {
+if ( -l "/var/www") {
 	print "$basedir already exists, no need do jump intial step!\n";
 } else {
 	mkdir $basedir;
